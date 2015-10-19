6e59cb9dcde16900c441cbe6e3026b4530cc44f6
SergeySavelyev <e1.nixz@gmail.com>
2015-10-20 02:35:07 +0300

    Create Monomial.java

---
+++ Polynomial/src/Monomial.java
@@ -0,0 +1,19 @@
+/**
+ * 
+ */
+
+/**
+ * @author Sergey Savelyev
+ *
+ */
+class Monomial {
+	
+	public double coefficient;
+	public int degree;
+	
+	Monomial (double coefficient, int degree) {
+		this.coefficient = coefficient;
+		this.degree = degree;
+	}
+
+}
